88b33750845785e4e81c7aed870826c258c939e7
treize <jonathan.rougier@hotmail.fr>
2025-07-08 09:39:08 +0200

    fix authorization header

--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -1,8 +1,11 @@
 use std::sync::Arc;
 
-use actix_web::{web, HttpResponse, Responder, Scope};
+use actix_web::{HttpResponse, Responder, Scope, http::header::HeaderName, web};
 
-use crate::user::{user_service::{CreateUserRequest, LoginRequest, UserService}, Error::{AlreadyExists, InvalidCredentials, InvalidInput}};
+use crate::user::{
+    Error::{AlreadyExists, InvalidCredentials, InvalidInput},
+    user_service::{CreateUserRequest, LoginRequest, UserService},
+};
 
 pub struct UserRoutes {
     user_service: Arc<UserService>,
@@ -10,9 +13,7 @@ pub struct UserRoutes {
 
 impl UserRoutes {
     pub fn new(user_service: Arc<UserService>) -> Self {
-        Self { 
-            user_service 
-        }
+        Self { user_service }
     }
 
     pub fn scope(data: web::Data<Self>) -> Scope {
@@ -22,7 +23,6 @@ impl UserRoutes {
             .route("/login", web::post().to(Self::login))
     }
 
-    // Define the handler methods here
     async fn register(data: web::Data<Self>, item: web::Json<CreateUserRequest>) -> impl Responder {
         match data.user_service.create_user(item.into_inner()).await {
             Ok(user_info) => HttpResponse::Created().json(user_info),
@@ -36,9 +36,17 @@ impl UserRoutes {
 
     async fn login(data: web::Data<Self>, item: web::Json<LoginRequest>) -> impl Responder {
         match data.user_service.login(item.into_inner()).await {
-            Ok(token) => HttpResponse::Ok().json(token),
+            Ok(token) => {
+                HttpResponse::Ok()
+                    .append_header(
+                        (HeaderName::from_static("authorization"), 
+                        format!("Bearer {}", token)
+                    ))
+                    .finish()
+            }
             Err(err) => match err {
                 InvalidCredentials(_) => HttpResponse::Unauthorized().finish(),
+                InvalidInput(_) => HttpResponse::BadRequest().finish(),
                 _ => HttpResponse::InternalServerError().finish(),
             },
         }
